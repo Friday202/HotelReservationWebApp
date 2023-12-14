298e38431bb84909d42cdc410d429d8e50de68fc
Friday202 <122792037+Friday202@users.noreply.github.com>
2023-12-14 14:11:20 +0100

    Added health checks.

--- catalog-service/src/main/java/si/petek/rso/catalogservice/controller/CatalogController.java
+++ catalog-service/src/main/java/si/petek/rso/catalogservice/controller/CatalogController.java
@@ -1,8 +1,10 @@
 package si.petek.rso.catalogservice.controller;
 
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import si.petek.rso.catalogservice.dto.CatalogResponse;
+import si.petek.rso.catalogservice.healthcheck.CatalogHealthIndicator;
 import si.petek.rso.catalogservice.service.CatalogService;
 
 import java.util.List;
@@ -11,9 +13,11 @@ import java.util.List;
 @RequestMapping("/api/v1/catalog")
 public class CatalogController {
     private final CatalogService catalogService;
+    private final CatalogHealthIndicator catalogHealthIndicator;
 
-    public CatalogController(CatalogService catalogService) {
+    public CatalogController(CatalogService catalogService, CatalogHealthIndicator catalogHealthIndicator) {
         this.catalogService = catalogService;
+        this.catalogHealthIndicator = catalogHealthIndicator;
     }
 
     @GetMapping()
@@ -27,4 +31,11 @@ public class CatalogController {
     public CatalogResponse getHotelById(@PathVariable Long hotelId){
         return catalogService.getHotelByID(hotelId);
     }
+
+    @PostMapping("/actuator/health/set")
+    public String simulateSickService(){
+        catalogHealthIndicator.setHealthStatus(Status.DOWN);
+        return "Simulating unhealthy state.";
+    }
+
 }
---
+++ catalog-service/src/main/java/si/petek/rso/catalogservice/healthcheck/CatalogHealthIndicator.java
@@ -0,0 +1,21 @@
+package si.petek.rso.catalogservice.healthcheck;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CatalogHealthIndicator implements HealthIndicator {
+
+    private Status healthStatus = Status.UP;
+
+    @Override
+    public Health health() {
+        return new Health.Builder(healthStatus).build();
+    }
+
+    public void setHealthStatus(Status healthStatus){
+        this.healthStatus = healthStatus;
+    }
+}
