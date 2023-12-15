29414153516a545e031f9fb34592214887b62d2d
Friday202 <122792037+Friday202@users.noreply.github.com>
2023-12-15 08:49:23 +0100

    Added healtchecks for inventory s.

--- inventory-service/src/main/java/si/petek/rso/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/si/petek/rso/inventoryservice/controller/InventoryController.java
@@ -1,8 +1,10 @@
 package si.petek.rso.inventoryservice.controller;
 
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import si.petek.rso.inventoryservice.dto.InventoryResponse;
+import si.petek.rso.inventoryservice.healthcheck.InventoryHealthCheck;
 import si.petek.rso.inventoryservice.service.InventoryService;
 
 @RestController
@@ -10,9 +12,11 @@ import si.petek.rso.inventoryservice.service.InventoryService;
 public class InventoryController {
 
     private final InventoryService inventoryService;
+    private final InventoryHealthCheck inventoryHealthCheck;
 
-    public InventoryController(InventoryService inventoryService) {
+    public InventoryController(InventoryService inventoryService, InventoryHealthCheck inventoryHealthCheck) {
         this.inventoryService = inventoryService;
+        this.inventoryHealthCheck = inventoryHealthCheck;
     }
 
     @GetMapping("/{hotel_id}")
@@ -26,5 +30,11 @@ public class InventoryController {
     public InventoryResponse getRoomAvailability(@PathVariable Long hotel_id, @PathVariable Long room_id){
         // Returns all dates that are taken for a particular room
         return null;
+    }
+
+    @PostMapping("/actuator/health/set")
+    public String simulateSickService(){
+        inventoryHealthCheck.setHealthStatus(Status.DOWN);
+        return "Simulating unhealthy state.";
     }
 }
--- inventory-service/src/main/java/si/petek/rso/inventoryservice/healthcheck/InventoryHealthCheck.java
+++ inventory-service/src/main/java/si/petek/rso/inventoryservice/healthcheck/InventoryHealthCheck.java
@@ -1,2 +1,20 @@
-package si.petek.rso.inventoryservice.healthcheck;public class InventoryHealthCheck {
+package si.petek.rso.inventoryservice.healthcheck;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InventoryHealthCheck implements HealthIndicator {
+    private Status healthStatus = Status.UP;
+
+    @Override
+    public Health health(){
+        return new Health.Builder(healthStatus).build();
+    }
+
+    public void setHealthStatus(Status healthStatus){
+        this.healthStatus = healthStatus;
+    }
 }
